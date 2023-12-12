450c41839cd7d00c36d3e171afff3495542844d7
Dimitrijevs <githubodinlv@gmail.com>
2023-12-12 20:26:13 +0200

    finished catalog + productCard

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1>Catalog</h1>
+    <div class="products-list">
+        <div class="product" v-for="product in store.products" :key="product.id" @click="goToProductPage(product.id)">
+            <img :src="product.thumbnail" alt="">
+            <h2>Brand: {{ product.title }}</h2>
+            <p>Description: {{ product.description }}</p>
+            <p>Price: {{ product.price }} €</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -13,15 +20,34 @@
 <script setup>
     import { onMounted } from 'vue';
     import { productsStore }  from "@/stores/products.js";
+    import { useRouter } from 'vue-router';
 
-    const productStore = productsStore();
+    const store = productsStore();
+    const router = useRouter()
+
+    const goToProductPage = (id) => {
+        router.push({ name: 'ProductView', params: { id }})
+    }
 
     onMounted(() => {
         console.log('onMounted')
-        productStore.fetchProductsFromDB()
+        store.fetchProductsFromDB()
     })
 </script>
 
-<style>
+<style scoped>
+    .products-list {
+        display: grid;
+        grid-template-columns: repeat(3, auto);
+        margin: 0 auto;
+        grid-gap: 40px;
+    }
 
+    .product {
+        cursor: pointer;
+    }
+
+    .product img {
+        max-width: 240px;
+    }
 </style>
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,44 @@
+<template>
+    <button @click="router.push({ name: 'Catalog' })">Back to catalog</button>
+    <div class="product">
+        <div class="product-image">
+            <img :src="selectedProduct.thumbnail" alt="">
+        </div>
+        <div class="product-details">
+            <p>Brand: {{ selectedProduct.brand }}</p>
+            <p>Description: {{ selectedProduct.description }}</p>
+            <p>Price: {{ selectedProduct.price }} €</p>
+            <button @click="addToCard">Add to card</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'ProductDetails',
+    })
+</script>
+
+<script setup>
+    import { computed } from 'vue';
+    import { productsStore } from '@/stores/products';
+    import { useRoute, useRouter} from 'vue-router';
+
+    const store = productsStore();
+    const router = useRouter();
+    const route = useRoute();
+
+    const selectedProduct = computed(() => {
+        return store.products.find((item) => item.id === Number(route.params.id))
+    })
+
+    const addToCard = () => {
+        console.log('test');
+    }
+</script>
+
+<style scoped>
+
+</style>
